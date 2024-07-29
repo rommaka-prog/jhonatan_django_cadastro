30eaac5b7df3118ecf6caaf3088402374d73af09
rommaka-prog <rommaka2024@gmail.com>
2024-07-29 02:57:43 +0000

    pronto

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from app_cad_usuarios import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    
     path('usuarios/', views.home, name='home'),
     path('usuarios/', views.usuarios, name='listagem_usuarios'),
 ]
