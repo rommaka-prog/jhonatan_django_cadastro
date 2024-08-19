0d1dd692c81c2b9b2c05154256a45e685d6d71ac
rommaka-prog <rommaka2024@gmail.com>
2024-08-19 21:33:51 +0000

    error

--- projeto_cad_usuarios/projeto_cad_usuarios/settings.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure--@m^c$21990nsfiv=j!#1m0bb+!z$=dzucq!a@a=raqndwj08j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    
+]
 
 
 # Application definition
